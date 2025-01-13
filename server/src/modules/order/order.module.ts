5e8b6dfe1ec38b6f9a3a2e7e96df329c508297b3
techfussion <muminraj15@gmail.com>
2025-01-13 12:32:27 +0100

    minor fix

--- server/src/modules/order/order.module.ts
+++ server/src/modules/order/order.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { OrderGateway } from './order.gateway';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   controllers: [OrderController],
   providers: [OrderService, OrderGateway],
   exports: [OrderService],
